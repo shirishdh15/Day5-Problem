fa6d399449acc942077579333cc76d625dced7cb
shirish <shirishdhalgade15@gmail.com>
2023-01-21 13:23:30 +0530

    LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,22 @@
+package Day5;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2020;
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
